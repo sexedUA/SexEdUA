a3a67e9206a292e4bb916a6cd3fd49ca8baddedb
atiragramk <beizmargo@gmail.com>
2023-08-03 14:26:30 +0300

    feat: started bot, set menu handler

---
+++ handlers/menu.py
@@ -0,0 +1,18 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Message
+
+
+talk = KeyboardButton('Поговоримо про секс? 🔥')
+quiz = KeyboardButton('Квізи для дорослих 😻')
+story = KeyboardButton('SexStories 😜')
+kamasutra = KeyboardButton('KAMACУТРА 💏')
+therapy = KeyboardButton('SexTherapy 💆‍♀️')
+subscribe = KeyboardButton('Підписатись на щоденний контент 🔔')
+need_help = KeyboardButton('Допомога ℹ️')
+
+
+menu_kb = ReplyKeyboardMarkup(
+    one_time_keyboard=True, resize_keyboard=True, keyboard=[[talk], [quiz, story], [kamasutra, therapy], [subscribe], [need_help]])
+
+
+async def menu_handler(message: Message):
+    await message.answer("Виберіть розділ", reply_markup=menu_kb)
