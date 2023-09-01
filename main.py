ce2dd6d320caf9916ea96fa11f3d6d69b0b107f8
atiragramk <beizmargo@gmail.com>
2023-09-01 09:08:13 +0300

    remove dummy server

--- main.py
+++ main.py
@@ -1,25 +1,6 @@
 import asyncio
 from main_bot import dp
 from admin_bot import dp as dp_admin
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import threading
-
-
-class DummyRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        # Replace with an appropriate message
-        self.wfile.write(b"Bot is running!")
-        return
-
-
-def run_dummy_server():
-    server_address = ('', 10000)  # Choose a port that's not used by your bot
-    httpd = HTTPServer(server_address, DummyRequestHandler)
-    print('Dummy server is running...')
-    httpd.serve_forever()
 
 
 async def run_bots():
@@ -30,8 +11,4 @@ async def run_bots():
     await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
-    dummy_thread = threading.Thread(target=run_dummy_server)
-    dummy_thread.start()
-    # asyncio.run(run_bots())
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_bots())
+    asyncio.run(run_bots())
